735ffc02ab04275861499a9605bb85275765d520
Ben Orenstein <ben.orenstein@gmail.com>
2014-09-29 17:03:22 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+ruby "2.1.3"
+
+gem "rspec", "~> 3.1"
+gem "rake"
---
+++ spec/user_spec.rb
@@ -0,0 +1,9 @@
+require_relative "../lib/person"
+
+describe Person do
+  describe "#full_name" do
+    it "returns the first and last names concatenated" do
+      # Write your spec here.
+    end
+  end
+end

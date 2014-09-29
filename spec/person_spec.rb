8990abb1317fbf8992d1d1e11b9b1c589a161c2f
Ben Orenstein <ben.orenstein@gmail.com>
2014-09-29 17:24:09 -0400

    Add Rakefile and instructions

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,6 +4,8 @@ describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
       # Write your spec here.
+      # You can delete the pending line.
+      pending "Not written yet"
     end
   end
 end

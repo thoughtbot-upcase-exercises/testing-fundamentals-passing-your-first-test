7aae7dba9db880c4b7e788c01c921465c8fdc1b4
Jamile Radloff <jamilemarcele@gmail.com>
2021-05-07 12:44:32 +0200

    first test upcase course

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,11 +1,12 @@
 require_relative "../lib/person"
 
 describe Person do
+  let(:person) { Person.new(first_name: 'Jamile', last_name: 'Radloff') }
+
   describe "#full_name" do
     it "returns the first and last names concatenated" do
       # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      expect(person.full_name).to eq('Jamile Radloff')
     end
   end
 end
